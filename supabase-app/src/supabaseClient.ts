15845939914f3a314c895ada74e18068c427da89
Ayushi Rajput <rajputayushi979@gmail.com>
2026-04-07 10:50:31 +0530

    final project

--- supabase-app/src/supabaseClient.ts
+++ supabase-app/src/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl: string = "https://oxbqxxjkafakrhaghkyj.supabase.co"
-const supabaseAnonKey: string = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im94YnF4eGprYWZha3JoYWdoa3lqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU0NzMzMDQsImV4cCI6MjA5MTA0OTMwNH0.mN4hcf_7gz9w21kO0BqGIfq25U1BOSucjQ2Q4N7QjsY"
+const supabaseUrl: string = import.meta.env.VITE_SUPABASE_URL as string
+const supabaseAnonKey: string = import.meta.env.VITE_SUPABASE_ANON_KEY as string
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
